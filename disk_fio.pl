c6abbac1cf34804eca9b39c90ecc74ab7a7a44ca
Joe Landman <landman@scalableinformatics.com>
2017-09-13 14:31:10 -0400

    Disk test scripts added:   disk_fio.pl:  build a fio script to test your disks (see usage in script)   disk_mkfsxfs.pl: create and mount 1 file system per disk of relevant                    type (see usage)

---
+++ disk_fio.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+
+
+# FIO testing generator
+#
+# usage:
+# ./disk_fio.pl /topmount Njob_per_LUN TYPE SIZE_in_GB BLOCKSIZE
+#
+# where /topmount is the mount point where you made the mounts
+#       Njob_per_LUN are the number of simultaneous jobs running against each mount point
+#       TYPE is read or write (or randread / randwrite )
+#       SIZE_in_GB is how much data to use across all jobs on each LUN
+#       BLOCKSIZE is the size of blocks we use 128k by default
+
+
+use strict;
+my ($dev,@paths,$entry,$fh,$header);
+use constant { true => (1==1), false=> (1==0) };
+
+my $toppath    = shift || "/data";
+
+# assuming LUNs are mounted under $toppath as $toppath/1 $toppath/2 ... $toppath/N
+chomp(@paths    = `ls $toppath`);
+
+# 1st arg is number of jobs (assuming 1) per LUN.  This sets concurrency
+my $njobs = shift || 1;
+
+# 2nd arg is read or write (default)
+my $mode  = shift || "write";
+
+# 3rd arg is size of each file in GB.  This will be divided by the number of
+# jobs to keep the read/write size the same
+my $size = shift || 10;
+
+my $blocksize = shift || "128k"
+
+$size/=$njobs;
+my $size_string = sprintf "%im",int($size*1024);
+
+my $fn = sprintf "%s_%s_%i_%i.fio",$mode,$njobs,$size,$blocksize;
+
+open($fh,">".$fn);
+$header = <<"EOF";
+[global]
+size=$size_string
+iodepth=32
+direct=1
+blocksize=$blocksize
+numjobs=$njobs
+nrfiles=1
+ioengine=vsync
+group_reporting
+loops=1
+create_serialize=0
+create_on_open=1
+rw=$mode
+
+EOF
+
+printf $fh $header;
+foreach $dev (@paths)
+  {
+   next if ($dev !~ /^\d+$/);
+   $entry = sprintf "$toppath/%s/test",$dev;
+   printf $fh "[%s]\ndirectory=$toppath/%s/test\n\n",$dev,$dev;
+   mkdir $entry;
+  }
+
+close($fh);
---
+++ disk_mkfsxfs.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+
+use strict;
+use constant { true => (1==1), false=> (1==0) };
+
+my ($dev,@devices,$cmd,$rc,$fh,$line,$count,$rotational,@disks);
+my ($mounted,$raided);
+
+my $toppath    = shift || "/data";
+my $userot     = shift || 1;  # 1 (disk) or 0 (ssd/nvme)
+
+my $mkfs_cmd = "mkfs.xfs -f -b size=4k -K -l size=128m -s size=4k /dev/%s";
+chomp(@devices    = `ls /sys/block`);
+$rc = `mkdir -p $toppath; umount -f -l $toppath/*`;
+$count=0;
+foreach $dev (@devices)
+  {
+   next if ($dev !~ /sd/i);
+   
+   # skip non-$userot media
+   chomp($rotational = `cat /sys/block/$dev/queue/rotational`);
+   next if ($rotational != $userot);
+   push @disks,$dev;
+  }
+
+printf "disks=%s\n",join(",",@disks);
+
+foreach $dev (@disks) {
+
+   # skip and disks that are mounted ...
+   chomp($mounted = `grep $dev /proc/mounts`); 
+   if ($mounted ne "") {
+      printf "skipping device %s: is mounted -> %s\n",$dev,$mounted;
+      next ;
+   }
+
+   # ... or disks that are part of an MD-RAID 
+   chomp($raided = `grep $dev /proc/mdstat`);
+   if ($raided ne "") {
+      printf "skipping device %s: is RAIDed -> %s\n",$dev,$raided;
+      next ;
+   }
+   
+   $cmd = (sprintf $mkfs_cmd,$dev);
+   printf "cmd=%s\n",$cmd;
+   open($fh,"$cmd |");
+   while($line = <$fh>) {
+	printf "%s: %s",$dev,$line;
+   }
+   close($fh);
+
+   # make mountpoint ...
+   $rc = `mkdir -p $toppath/$count`;
+
+   # mount disk
+   $rc = `mount -o inode64,noatime,nodiratime,logbufs=8,logbsize=256k /dev/$dev $toppath/$count`;
+   printf "\n=====\nrc= %s\n-----\n",$rc;
+   
+   $count++;   
+  }
